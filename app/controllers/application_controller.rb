54326d877cd2f58d2e951f983c5c633935c8f81f
Vishal Mahulkar <vishal.mahulkar@gmail.com>
2019-11-02 19:05:37 +0000

    route + director

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+
+  def youngest_director
+
+    youngest = Director.youngest
+
+    render({ :plain => youngest.to_json })
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-
+  match("/directors/youngest",{:controller => "application", :action => "youngest_director", :via => "get"})
 
   # ================================================
   devise_for :admin_users, ActiveAdmin::Devise.config
