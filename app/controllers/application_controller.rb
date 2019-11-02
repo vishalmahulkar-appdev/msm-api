aa97e5c808effe3ab37642caf30b8e94960db910
Vishal Mahulkar <vishal.mahulkar@gmail.com>
2019-11-02 20:12:12 +0000

    delete records

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -46,8 +46,8 @@ class ApplicationController < ActionController::Base
   def movie_cast
     m = Movie.where( {:id => params.fetch(:movie_id) }).at(0)
     answer = m.cast
-    newanswer = {:movie => m, :cast => answer}
-    render({ :plain => newanswer.to_json })
+    #answer = {:movie => m, :cast => answer}
+    render({ :plain => answer.to_json })
   end
 
   def actor_characters
@@ -59,6 +59,46 @@ class ApplicationController < ActionController::Base
   def actor_filmography
     a = Actor.where( {:id => params.fetch(:actor_id) }).at(0)
     answer = a.filmography
+    render({ :plain => answer.to_json })
+  end
+
+  def delete_movie
+    m = Movie.where( {:id => params.fetch(:id) }).at(0)
+    cb = Movie.count
+    m.destroy
+    ca = Movie.count
+    answer = {:count_before => cb, :count_after => ca, :deleted_movie => m}
+
+    render({ :plain => answer.to_json })
+  end
+
+  def delete_actor
+    a = Actor.where( {:id => params.fetch(:id) }).at(0)
+    cb = Actor.count
+    a.destroy
+    ca = Actor.count
+    answer = {:count_before => cb, :count_after => ca, :deleted_actor => a}
+
+    render({ :plain => answer.to_json })
+  end
+
+  def delete_director
+    d = Director.where( {:id => params.fetch(:id) }).at(0)
+    cb = Director.count
+    d.destroy
+    ca = Director.count
+    answer = {:count_before => cb, :count_after => ca, :deleted_director => d}
+
+    render({ :plain => answer.to_json })
+  end
+
+  def delete_character
+    c = Character.where( {:id => params.fetch(:id) }).at(0)
+    cb = Character.count
+    c.destroy
+    ca = Character.count
+    answer = {:count_before => cb, :count_after => ca, :deleted_character => c}
+
     render({ :plain => answer.to_json })
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,28 @@
 Rails.application.routes.draw do
 
+  #Static
   match("/directors/youngest",{:controller => "application", :action => "youngest_director", :via => "get"})
   match("/directors/eldest",{:controller => "application", :action => "eldest_director", :via => "get"})
   match("/movies/last_decade",{:controller => "application", :action => "last_decade_movies", :via => "get"})
   match("/movies/short",{:controller => "application", :action => "short_movies", :via => "get"})
   match("/movies/long",{:controller => "application", :action => "long_movies", :via => "get"})
+
+  #Dynamic
   match("/directors/:director_id/filmography", { :controller => "application", :action => "director_filmography", :via => "get" })
   match("/movies/:movie_id/director", { :controller => "application", :action => "movie_director", :via => "get" })
   match("/movies/:movie_id/characters", { :controller => "application", :action => "movie_characters", :via => "get" })
   match("/movies/:movie_id/cast", { :controller => "application", :action => "movie_cast", :via => "get" })
   match("/actors/:actor_id/characters", { :controller => "application", :action => "actor_characters", :via => "get" })
   match("/actors/:actor_id/filmography", { :controller => "application", :action => "actor_filmography", :via => "get" })
+
+  #Delete
+  match("/delete_movie/:id", { :controller => "application", :action => "delete_movie", :via => "get" })
+  match("/delete_director/:id", { :controller => "application", :action => "delete_director", :via => "get" })
+  match("/delete_actor/:id", { :controller => "application", :action => "delete_actor", :via => "get" })
+  match("/delete_character/:id", { :controller => "application", :action => "delete_character", :via => "get" })
+
+
+
 
   # ================================================
   devise_for :admin_users, ActiveAdmin::Devise.config
